2754646cd364b6bd6015ac56a9a47b48ffee18dd
Sebastian <sebastian.chang23@gmail.com>
2021-08-25 11:45:05 -0500

    Initial commit

---
+++ src/components/ClassCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Class Card</h1>
+</template>
+
+<script>
+import axios from 'axios'
+import { onMounted } from 'vue'
+
+export default {
+  setup () {
+    // Mounted
+    onMounted(() => {
+      axios
+        .get('http://localhost:8000/classes/')
+        .then(res => console.log('this is the response ', res))
+    })
+  },
+
+}
+</script>
+
+<style>
+</style>
